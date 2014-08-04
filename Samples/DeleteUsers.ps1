972cc58e80e9887d516e1c2983da827c1f5ba357
Peter Schulz <schulzpm@Peters-MacBook-Pro-2.local>
2014-08-04 13:17:07 -0400

    Added sample scripts

---
+++ Samples/DeleteUsers.ps1
@@ -0,0 +1,19 @@
+﻿#Script to delete users found by the Disabled Users script
+
+
+Function DeleteUsers{
+Param ([Parameter(HelpMessage="Hi Keith")] [string]$UserType="employee")
+      $client = New-SfClient -Name "c:\tmp\sfclient.sfps"
+
+      $sfUserObjects = Import-Csv ("C:\tmp\" + $UserType + ".csv")
+
+      foreach($sfUser in $sfUserObjects){
+           Send-SfRequest -Client $client -Method Delete -Entity Users -Id $sfUser.UserId -Parameters @{"completely" = "true"}
+      }
+
+}
+
+#New-SfClient -Name "c:\tmp\sfclient.sfps"
+Add-PSSnapin ShareFile
+DeleteUsers -UserType "employee";
+DeleteUsers -UserType "client";
---
+++ Samples/DisabledUsers.ps1
@@ -0,0 +1,44 @@
+﻿#Get all disabled users
+
+Add-PSSnapIn ShareFile
+
+Function FindDisabledUsers{
+Param ( [string]$UserType="employee")
+      $client = Get-SfClient -Name "c:\tmp\sfclient.sfps"
+      $entity = "";
+      switch ($UserType.ToLower()){
+        "employee"{
+            $entity = 'Accounts/Employees';
+        } 
+        "client" {
+          $entity = 'Accounts/Clients';
+        }
+      }
+      #Pull all of the Account Employees or Clients
+      $sfUsers = Send-SfRequest -Client $client -Entity $entity
+
+      $fileOutput = @()
+      #Loop through each of the Employees or Clients returned from inital call
+      foreach($sfUserId in $sfUsers){
+            #Get full user information including security 
+            $sfUser = Send-SfRequest -Client $client -Entity Users -Id $sfUserId.Id -Expand Security
+            #Output to Console Emails of disabled users
+            Write-Host $sfUser.Email
+            #check to see if security parameter IsDisabled is true
+            switch ($sfUser.Security.IsDisabled ) {
+                "True" {
+                    $fileOutput += New-Object PSObject -Property @{'UserId'=$sfUserId.Id;'FullName'=$sfUser.FullName;'Email'=$sfUser.Email}
+                }
+            }
+      }
+
+    #Output CSV file with all disabled user information
+    $fileOutput | Export-Csv ("C:\tmp\" + $UserType + ".csv") -Force -NoTypeInformation
+}
+
+#Comment out this line after the first run in order to use the saved authentication file
+New-SfClient -Name "c:\tmp\sfclient.sfps"
+#Get-SfClient -Name "c:\tmp\sfclient.sfps"
+
+FindDisabledUsers -UserType "employee";
+FindDisabledUsers -UserType "client";
