6e188f47dbebab36a385834dcda83457a06f3408
asimiqbal11 <asim.iqbal@ebricks-inc.com>
2015-05-06 18:08:34 +0500

    Committing intial changes for Filter parameter for SendSfRequest

--- ShareFileSnapIn/SendSfRequest.cs
+++ ShareFileSnapIn/SendSfRequest.cs
@@ -10,6 +10,7 @@ using System.IO;
 using ShareFile.Api.Client.Requests;
 using Newtonsoft.Json;
 using ShareFile.Api.Client.Exceptions;
+using ShareFile.Api.Client.Requests.Filters;
 
 namespace ShareFile.Api.Powershell
 {
@@ -78,6 +79,7 @@ namespace ShareFile.Api.Powershell
             Query<ODataObject> query = new Query<ODataObject>(Client.Client);
             query.HttpMethod = Method;
 
+            if (string.IsNullOrWhiteSpace(Filter) == false) query.Filter(AddFilter());
             if (Entity != null) query = query.From(Entity);
             if (Action != null) query = query.Action(Action);
             if (Id != null) query = query.Id(Id);
@@ -128,6 +130,241 @@ namespace ShareFile.Api.Powershell
             {
                 WriteError(new ErrorRecord(new Exception(e.Code.ToString() + ": " + e.ODataExceptionMessage.Message), e.Code.ToString(), ErrorCategory.NotSpecified, query.GetEntity()));
             }
+        }
+
+        private IFilter AddFilter()
+        {
+            FilterBuilder builder = new FilterBuilder(Filter);
+            return builder.Build();
+        }
+
+
+        private enum OperatorType
+        {
+            eq,
+            ne,
+            gt,
+            ge,
+            lt,
+            le,
+            and,
+            or,
+            not,
+            add,
+            sub,
+            mul,
+            div,
+            mod,
+
+            startswith,
+            endswith,
+            substr,
+            type
+        }
+
+        private class FilterBuilder
+        {
+
+            private Stack<string> OperatorsStack;
+            private Stack<string> PropertiesStack;
+            private string FilterBody;
+
+            public FilterBuilder(string filterBody)
+            {
+                OperatorsStack = new Stack<string>();
+                PropertiesStack = new Stack<string>();
+                FilterBody = filterBody;
+            }
+
+            public IFilter Build()
+            {
+                SplitText(FilterBody);
+                return GetFilter();
+            }
+
+            private void SplitText(string filterText)
+            {
+                string[] sepSpace = { " " };
+                string[] sepBraces = { "(", ",", ")" };
+                string[] result = null;
+
+                string propertyName = string.Empty;
+                string filter = string.Empty;
+                string propertyValue = string.Empty;
+
+
+                if (IsNotOperator(filterText))
+                {
+                    OperatorsStack.Push("not");
+                    filterText = filterText.Substring(4).Trim();
+                }
+
+                if (IsBracesOperator(filterText))
+                {
+                    result = filterText.Split(sepBraces, 4, StringSplitOptions.RemoveEmptyEntries);
+
+                    filter = result[0].Trim();
+                    propertyName = result[1].Trim();
+                    propertyValue = result[2].Trim();
+
+                    if (result.Length == 4)
+                    {
+                        propertyValue += result[3];
+                    }
+                }
+                else
+                {
+                    result = filterText.Split(sepSpace, 3, StringSplitOptions.RemoveEmptyEntries);
+
+                    propertyName = result[0].Trim();
+                    filter = result[1].Trim();
+                    propertyValue = result[2].Trim();
+                }
+
+                string[] nextFilter = MoveToNext(propertyValue);
+                if (nextFilter != null && nextFilter.Length == 3)
+                {
+                    propertyValue = nextFilter[0];
+                    OperatorsStack.Push(nextFilter[1]);
+                    SplitText(nextFilter[2]);
+                }
+
+                OperatorsStack.Push(filter);
+                PropertiesStack.Push(propertyName);
+                PropertiesStack.Push(propertyValue);
+
+            }
+
+            private bool IsNotOperator(string filterText)
+            {
+                return filterText.StartsWith("not ", StringComparison.CurrentCultureIgnoreCase);
+            }
+
+            private bool IsBracesOperator(string filterText)
+            {
+                return filterText.StartsWith("startswith(", StringComparison.CurrentCultureIgnoreCase)
+                    || filterText.StartsWith("endswith(", StringComparison.CurrentCultureIgnoreCase);
+            }
+
+            private string[] MoveToNext(string filterText)
+            {
+                string[] array = null;
+                string remainingText = null;
+                string logicalOperator = null;
+
+                if (filterText.StartsWith("'") || filterText.StartsWith("\""))
+                {
+                    char delimeter = filterText.ElementAt(0);
+
+                    logicalOperator = filterText.Substring(0, filterText.IndexOf(delimeter, 1)).Trim();
+                    remainingText = filterText.Substring(filterText.IndexOf(delimeter, 1) + 1).Trim();
+                }
+                else if (filterText.IndexOf(" ") > -1)
+                {
+                    logicalOperator = filterText.Substring(0, filterText.IndexOf(" ")).Trim();
+                    remainingText = filterText.Substring(filterText.IndexOf(" ")).Trim();
+                }
+
+                if (!string.IsNullOrWhiteSpace(remainingText))
+                {
+                    array = new string[3];
+                    array[0] = logicalOperator;
+                    array[1] = remainingText.Substring(0, remainingText.IndexOf(" "));
+                    array[2] = remainingText.Substring(remainingText.IndexOf(" ") + 1);
+                }
+
+                return array;
+            }
+
+            private IFilter GetFilter()
+            {
+                IFilter filterType = null;
+
+                Stack<IFilter> Operations = new Stack<IFilter>();
+
+                while (Operations.Count > 0)
+                {
+                    string propertyName = null;
+                    string propertyValue = null;
+                    IFilter left = null;
+                    IFilter right = null;
+                    string op = OperatorsStack.Pop();
+                    OperatorType operators = (OperatorType)Enum.Parse(typeof(OperatorType), op, true);
+
+                    switch (operators)
+                    {
+                        case OperatorType.eq:
+                        case OperatorType.ne:
+                        case OperatorType.startswith:
+                        case OperatorType.endswith:
+                        case OperatorType.lt:
+                        case OperatorType.le:
+                        case OperatorType.gt:
+                        case OperatorType.ge:
+                            propertyValue = PropertiesStack.Pop();
+                            propertyName = PropertiesStack.Pop();
+
+                            filterType = CreateFilter(operators, propertyName, propertyValue);
+                            break;
+
+                        case OperatorType.and:
+                            right = Operations.Pop();
+                            left = Operations.Pop();
+                            filterType = new AndFilter(left, right);
+                            break;
+
+                        case OperatorType.or:
+                            right = Operations.Pop();
+                            left = Operations.Pop();
+                            filterType = new AndFilter(left, right);
+                            break;
+
+                        case OperatorType.not:
+                            right = Operations.Pop();
+                            filterType = new NotFilter(right);
+                            break;
+                    }
+
+                    Operations.Push(filterType);
+                }
+
+                return Operations.Pop();
+            }
+
+            private static IFilter CreateFilter(OperatorType operators, string propertyName, string propertyValue)
+            {
+                IFilter filter = null;
+                switch (operators)
+                {
+                    case OperatorType.eq:
+                        filter = new EqualToFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.ne:
+                        filter = new NotEqualToFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.startswith:
+                        filter = new StartsWithFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.endswith:
+                        filter = new EndsWithFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.lt:
+                        filter = new LessThanFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.le:
+                        filter = new LessThanOrEqualFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.gt:
+                        filter = new GreaterThanFilter(propertyName, propertyValue);
+                        break;
+                    case OperatorType.ge:
+                        filter = new GreaterThanOrEqualFilter(propertyName, propertyValue);
+                        break;
+                }
+
+                return filter;
+            }
+
         }
     }
 }
