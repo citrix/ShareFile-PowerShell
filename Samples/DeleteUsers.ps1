b331a44752a3e767fe2df42030dae1d18bf5b0f8
Meg Rehn <mrehn87@gmail.com>
2019-07-01 22:00:04 -0400

    Suggest folder reassignment on user deletion.

--- Samples/DeleteUsers.ps1
+++ Samples/DeleteUsers.ps1
@@ -1,19 +1,32 @@
-﻿#Script to delete users found by the Disabled Users script
+﻿#Script to delete users and reassign items and groups
 
 
 Function DeleteUsers{
-Param ([Parameter(HelpMessage="Hi Keith")] [string]$UserType="employee")
+
+#Optional define parameters on function
+Param ( [Parameter(Mandatory, HelpMessage="Please define the UserType - client or employee")][string]$UserType,
+        [Parameter(Mandatory, HelpMessage="Enter a UserID to reassign user data to [Format:GUID]")][string]$UserID_ItemReassign,
+        [Parameter(Mandatory, HelpMessage="Enter a UserID to eassign user groups to [Format:GUID]")][string]$UserID_GroupReassign
+       )
+
       $client = New-SfClient -Name "c:\tmp\sfclient.sfps"
 
       $sfUserObjects = Import-Csv ("C:\tmp\" + $UserType + ".csv")
 
-      foreach($sfUser in $sfUserObjects){
-           Send-SfRequest -Client $client -Method Delete -Entity Users -Id $sfUser.UserId -Parameters @{"completely" = "true"}
+      foreach($sfUser in $sfUserObjects)
+      {
+           Send-SfRequest -Client $client -Method Delete -Entity Users -Id $sfUser.UserId -Parameters @{"completely" = "true"; "itemsReassignTo" = "$UserID_ItemReassign"; "groupsReassignTo" = "$UserID_GroupReassign"}
       }
 
 }
 
 #New-SfClient -Name "c:\tmp\sfclient.sfps"
 Add-PSSnapin ShareFile
-DeleteUsers -UserType "employee";
-DeleteUsers -UserType "client";
+
+
+#Alternatively Read-Host into variables
+$vUserType = Read-Host -Prompt 'What user type will be deleted? (employee/client)'
+$vUserID_ItemReassign = Read-Host -Prompt 'Enter a UserID to reassign user data to [Format:GUID]'
+$vUserID_GroupReassign = Read-Host -Prompt 'Enter a UserID to eassign user groups to [Format:GUID]'
+
+DeleteUsers -UserType $vUserType -UserID_ItemReassign $vUserID_ItemReassign -UserID_GroupReassign $vUserID_GroupReassign;
